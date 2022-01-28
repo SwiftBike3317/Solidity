7792a0833146791ec139372b9ebd8af26347d84c
SwiftBike3317 <95043476+SwiftBike3317@users.noreply.github.com>
2022-01-29 01:37:09 +0300

    Delete WaveSwap directory

--- WaveSwap/WaveSwap/migrations/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-var WaveToken = artifacts.require("WaveToken");
-var MasterChef = artifacts.require("MasterChef");
-module.exports = function(deployer) {
-    deployer.deploy(WaveToken)
-    deployer.deploy(MasterChef, "0x4259daA748B850e6fa69b66e3cF4240Af4faB86c", "0x7443e7E099a969ca075AE6d0e6411fe9a1A4D18B", "0x7443e7E099a969ca075AE6d0e6411fe9a1A4D18B", 10000, 11241000);
-};
